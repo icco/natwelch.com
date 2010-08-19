3b9af764c23ce85ce5093c33b4f1578677340ef7
Nat Welch <nat@natwelch.com>
2010-08-18 22:32:04 -0700

    add dave to friends

--- friends.php
+++ friends.php
@@ -1,28 +1,26 @@
 <html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head profile="http://gmpg.org/xfn/11">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Nat Welch Dot Com > Friends</title>
-<meta name="Keywords" content="Nat, welch, natwelch, nathaniel, nathanielwelch, dehn, code, student, developer, software" />
-<link href="includes/css/style.css" rel="stylesheet" type="text/css" />
-</head>
-<body>
-<div id="card">
-<h1>My Friends Online</h1>
-<div id="backButton"><a href="." title="natwelch.com">&lt;&lt; Go Back Home</a></div>
-    <ul id="links">
-    <li><a href="http://prenticew.com" rel="friend met co-resident" title="My Good Friend and Roomate Prentice's Site">Prentice Wongvibulsin</a> - Roommate and a great code and photographer.</li>
+   <head profile="http://gmpg.org/xfn/11">
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <title>Nat Welch Dot Com > Friends</title>
+      <meta name="Keywords" content="Nat, welch, natwelch, nathaniel, nathanielwelch, dehn, code, student, developer, software" />
+      <link href="includes/css/style.css" rel="stylesheet" type="text/css" />
+   </head>
+   <body>
+      <div id="card">
+         <h1>My Friends Online</h1>
+         <div id="backButton"><a href="." title="natwelch.com">&lt;&lt; Go Back Home</a></div>
+         <ul id="links">
+            <li><a href="http://prenticew.com" rel="friend met co-resident" title="My Good Friend and Roomate Prentice's Site">Prentice Wongvibulsin</a> - Roommate and a great code and photographer.</li>
+            <li><a href="http://www.wiredgoats.com/" rel="friend met" title="Dustin's secret site of nothing">Doc Mollo</a> - The man who got me started in Linux and a great friend.</li>
+            <li><a href="http://mgius.livejournal.com/" title="mgius's Livejournal. Angst Ensues" rel="friend co-resident met" >Mark Gius</a> - Roommate and good friend.</li>
+            <li><a href="http://users.csc.calpoly.edu/~boppenhe/" title="Brian's Webpage" rel="friend met">Brian Oppenheim</a> - Probably one of the most diligent and impressive coders I know.</li>
+            <li><a href="http://www.lydiadehn.com/" title="Mom" rel="met parent">Lydia Dehn</a> - My mom and her Art.</li>
+            <li><a href="http://dmpatierno.com/" rel="friend met co-worker" title="dave patierno">Dave Patierno</a> - Good friend and coworker.</li>
 
-    <li><a href="http://www.wiredgoats.com/" rel="friend met" title="Dustin's secret site of nothing">Doc Mollo</a> - The man who got me started in linux and a great friend.</li>
-    <li><a href="http://ErikCarpenter.com" rel="friend met" title="Erik, AKA Urban.">Urban</a> - If the OS exists, he probably installed it and didn't like it.</li>
-    <li><a href="http://mgius.livejournal.com/" title="mgius's Livejournal. Angst Ensues" rel="friend co-resident met" >Mark Gius</a> - Roommate and good friend.</li>
-    <li><a href="http://users.csc.calpoly.edu/~mwooding/" title="Matt's Little Space of the internet" rel="friend co-resident met">Matt Wooding</a> - Roommate, Friend, Artist and Star Wars enthusiast.</li>
-    <li><a href="http://users.csc.calpoly.edu/~boppenhe/" title="Brian's Webpage" rel="friend met">Brian Oppenheim</a> - Probably one of the most diligent and impressive coders I know.</li>
-    <li><a href="http://www.lydiadehn.com/" title="Mom" rel="met parent">Lydia Dehn</a> - My mom and her Art.</li>
-    <!-- Commented out until he gets his act together...
-    <li><a href="http://www.dcwelch.com/" rel="met parent">David Welch</a></li>
-    -->
-    </ul>
-</div>
-</body>
+            <!-- Commented out until he gets his act together...
+            <li><a href="http://www.dcwelch.com/" rel="met parent">David Welch</a></li>
+            -->
+         </ul>
+      </div>
+   </body>
 </html>
