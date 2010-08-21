642197b79fa67c6a0d1a4ee457c6646f2984f6cd
Nat Welch <nat@natwelch.com>
2010-08-21 15:25:43 -0700

    Trying out a little keyboard nav

--- includes/js/nat.js
+++ includes/js/nat.js
@@ -23,16 +23,55 @@ var Nat = {
       s.sort(idx_list);
    },
 
+   move : function (nextid) {
+      datas = $$('.data');
+      datas.each(function (elm) {
+         if (elm.id == 'data' + nextid) {
+            elm.show();
+         } else {
+            elm.hide();
+         }
+      });
+   },
+
    updateAge : function () {
       // Make age accurate
       bday = new Date(1988,1,22,17);
       diff = bday.diff(new Date());
       $('age').set('text', (diff / 365.4).toFixed(2));
+   },
+
+   lastfm : function () {
+      // Not working yet
+      // API: c8a55898b287950c836a1af12d91ce7d
+      var reqUrl = 'http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=icco&api_key=c8a55898b287950c836a1af12d91ce7d';
+      var req = new Request({
+         url : reqUrl,
+         isSuccess : function (text, xml) {
+            console.log(xml);
+            console.log(text);
+         }
+      });
+      req.send({
+         method: 'get'
+      });
    }
 };
 
 window.addEvent("domready", function() {
    Nat.updateAge();
    konami = new Konami(function(){ Nat.sort(); });
+
+   // Keyboard navigation
+   var curid = 1;
+   window.addEvent('keypress', function (ev) {
+      if (ev.key == 'right') {
+         curid = curid >= $$('.data').length ? $$('.data').length : (curid + 1);
+         Nat.move(curid);
+      } else if (ev.key == 'left') {
+         curid = curid <= 1 ? 1 : (curid - 1);
+         Nat.move(curid);
+      }
+   });
 });
 
