5a5fbc2c99703bcff904e5c406a18bb39f7b092a
Nat Welch <nat@natwelch.com>
2010-09-12 20:42:06 -0700

    Whoa there, this page is broken.

--- code/index.php
+++ code/index.php
@@ -1,3 +1,8 @@
+
+<?php 
+header("Location: http://icco.github.com/");
+/*
+
 <?php include "../PHP/v4.generics.php"; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -53,4 +58,6 @@ become a statistic. You have been warned ....... Tyler." - Tyler Durden, Fight
 Club DVD -->
 </body>
 </html>
+
+ */ ?>
 
