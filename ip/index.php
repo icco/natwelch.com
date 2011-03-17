81563c6d0c844c8a74c38b6bbc42641ee56e9291
Nat Welch <nat@natwelch.com>
2011-03-17 13:04:42 -0700

    We don't need no stinking html.

--- ip/index.php
+++ ip/index.php
@@ -1,16 +1,13 @@
 <?php
+
+// Print out visitors IP address.
+
 if ($_SERVER['HTTP_X_FORWARD_FOR']) {
    $ip = $_SERVER['HTTP_X_FORWARD_FOR'];
 } else {
    $ip = $_SERVER['REMOTE_ADDR'];
 }
-?>
 
-<html>
-<head>
-<title>Your IP</title>
-</head>
-<body>
-<?php echo $ip; ?>
-</body>
-</html>
+header("Content-type: text/plain-text");
+echo $ip;
+?>
