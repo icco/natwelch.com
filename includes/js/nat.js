42b590b466be3fbe99f59e5b5ea557318c46a16d
Nat Welch <nat@natwelch.com>
2011-12-11 23:06:08 -0800

    Whee, redesign!

--- includes/js/nat.js
+++ includes/js/nat.js
@@ -2,43 +2,28 @@
  * JS for natwelch.com
  */
 var Nat = {
-   sort : function () {
-      // Sorts the link list
-      var s = new Fx.Sort($$('ul#melinks li'));
-      var list = $$('ul#melinks li');
-      var text_list = new Array();
-      var idx_list = new Array();
+  updateAge : function () {
+    // Make age accurate
+    bday = new Date(1988,1,22,17);
+    diff = bday.diff(new Date());
+    $('age').set('text', (diff / 365.4).toFixed(2));
+  },
 
-      list.each(function (x) {
-         text_list.push(x.get('text').trim());
-      });
+  expand : function () {
+    $('more-data').show();
+    $('expander').hide();
+    $('buffer').setStyle('height', '150px');
+  },
 
-      var sort_list = $A(text_list);
-
-      sort_list.sort();
-      sort_list.each(function (x) {
-         idx_list.push(text_list.indexOf(x));
-      });
-
-      s.sort(idx_list);
-   },
-
-   updateAge : function () {
-      // Make age accurate
-      bday = new Date(1988,1,22,17);
-      diff = bday.diff(new Date());
-      $('age').set('text', (diff / 365.4).toFixed(2));
-   },
-
-   changeBG : function () {
-      document.body.setStyles({
-         backgroundImage: 'url("images/konamipower.gif")'
-      });
-   },
+  changeBG : function () {
+    document.body.setStyles({
+      backgroundImage: 'url("/i/konamipower.gif")'
+    });
+  },
 };
 
 window.addEvent("domready", function() {
-   Nat.updateAge();
-   konami = new Konami(function() { Nat.changeBG(); });
+  Nat.updateAge();
+  konami = new Konami(function() { Nat.changeBG(); });
 });
 
