b5a823ac2cfa6c398ac7a288e8f998858cebe18a
Nat Welch <nat@natwelch.com>
2012-03-27 22:56:01 -0700

    Nat Welch dot com version 13.

--- includes/js/nat.js
+++ includes/js/nat.js
@@ -9,15 +9,11 @@ var Nat = {
     $('age').set('text', (diff / 365.4).toFixed(2));
   },
 
-  expand : function () {
-    $('more-data').show();
-    $('expander').hide();
-    $('buffer').setStyle('height', '150px');
-  },
-
   changeBG : function () {
     document.body.setStyles({
-      backgroundImage: 'url("/i/konamipower.gif")'
+      backgroundImage: 'url("/i/konamipower.gif")',
+      backgroundRepeat: 'repeat',
+      backgroundSize: 'auto'
     });
   },
 };
