52c26a09be4608a20c71b041ddd112ed9222f302
Nat Welch <nat@natwelch.com>
2014-01-03 11:40:32 -0800

    merge for dmp

--- js/nat.js
+++ js/nat.js
@@ -18,3 +18,23 @@ $(function() {
 
   // Nat.updateAge();
 });
+
+var _gauges = _gauges || [];
+(function() {
+  var t   = document.createElement('script');
+  t.type  = 'text/javascript';
+  t.async = true;
+  t.id    = 'gauges-tracker';
+  t.setAttribute('data-site-id', '5213bb1df5a1f5312100001b');
+  t.src = '//secure.gaug.es/track.js';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(t, s);
+})();
+
+var _gaq = [['_setAccount', 'UA-333449-2'], ['_trackPageview']];
+(function(d, t) {
+  var g = d.createElement(t), s = d.getElementsByTagName(t)[0];
+  g.async = true;
+  g.src = '//www.google-analytics.com/ga.js';
+  s.parentNode.insertBefore(g, s);
+})(document, 'script');
