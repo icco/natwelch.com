5647f64c1cee7482f95642678376e59ef0968585
Nat Welch <nat@natwelch.com>
2014-05-16 12:13:11 -0700

    ics to checkins

--- 2014/4sq/foursquare_archive.rb
+++ 2014/4sq/foursquare_archive.rb
@@ -30,34 +30,40 @@ foursq = Foursquare2::Client.new(
   :client_secret => 'E5JK41TYFPG2YMPHG30WMMR0M3R4NMTPWEOSHOPVQONMED3E',
 )
 
-check_ins = foursq.user_checkins({})
-L.info("Began parsing #{check_ins.canonicalUrl}")
+cal_file = File.open("JB2YG3WJSXZH0I3WHY2CNALAXYL4YWEX.ics")
+
+# Parser returns an array of calendars because a single file
+# can have multiple calendars.
+cals = Icalendar.parse(cal_file)
+cal = cals.first
+L.info("Began parsing #{cal.events.count} events.")
 
 # Begin going through the data.
-#simplified_data = {}
-#list_data.listItems.items.each do |place|
-#  L.debug("#{place.venue.name.inspect}: #{place.venue.location.to_hash}")
-#  # NOTE: We don't use from_latlong becuase that expects formats like 22`34
-#  # 23.45N while we get stuff like 51.5168466460692 from 4sq.
-#  simplified_data[place.venue.name] = GeoRuby::SimpleFeatures::Point.from_lon_lat(place.venue.location.lng, place.venue.location.lat)
-#end
-#
-## Now we make the hash look like geojson.
-#feature_data = simplified_data.to_a.map do |name, geometry|
-#  {
-#    type: "Feature",
-#    geometry: geometry,
-#    properties: { name: name },
-#  }
-#end
-#
-## And now we construct a list of geojson "features"
-#output_hash = {
-#  type: "FeatureCollection",
-#  features: feature_data
-#}
-#
-## And now WE WRITE THAT MOTHER FUCKER TO DISK BECAUSE WE ARE BINARY GODS.
-#filename = "#{filename}.geojson"
-#L.info("Writing #{list_data.canonicalUrl} to #{filename}")
-#File.open(filename, 'w') { |file| file.write(output_hash.to_json) }
+simplified_data = {}
+cal.events.each do |event|
+  # NOTE: We don't use from_latlong becuase that expects formats like 22`34
+  # 23.45N while we get stuff like 51.5168466460692 from 4sq.
+  simplified_data[event.location] = GeoRuby::SimpleFeatures::Point.from_lon_lat(event.geo[0], event.geo[1])
+end
+
+# Now we make the hash look like geojson.
+feature_data = simplified_data.to_a.map do |name, geometry|
+  {
+    type: "Feature",
+    geometry: geometry,
+    properties: {
+      name: name,
+    },
+  }
+end
+
+# And now we construct a list of geojson "features"
+output_hash = {
+  type: "FeatureCollection",
+  features: feature_data
+}
+
+# And now WE WRITE THAT MOTHER FUCKER TO DISK BECAUSE WE ARE BINARY GODS.
+filename = "checkins.geojson"
+L.info("Writing to #{filename}")
+File.open(filename, 'w') { |file| file.write(output_hash.to_json) }
