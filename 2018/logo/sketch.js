e37cf9b619fcf3f823bbe7f65901097b142c544e
Nat Welch <nat@natwelch.com>
2018-06-07 21:51:24 +0000

    works

--- 2018/logo/sketch.js
+++ 2018/logo/sketch.js
@@ -1,5 +1,4 @@
-
-  var r = random(50);
+let t = 0; // time variable
 
 function setup() {
   createCanvas(400, 400);
@@ -7,13 +6,28 @@ function setup() {
 }
 
 function draw() {
-  stroke(r * 5);
-  strokeWeight(8);
+  background(10, 10); // translucent background (creates trails)
 
-  var radi = 75
+  [
+    [ 50, 50 ],
+    [ 150, 50 ],
+    [ 50, 150 ],
+    [ 150, 150 ]
+  ].forEach(function(arr) {
+    let x = arr[0]
+    let y = arr[1]
+    // starting point of each circle depends on mouse position
+    let xAngle = map(mouseX, 0, width, -4 * PI, 4 * PI, true);
+    let yAngle = map(mouseY, 0, height, -4 * PI, 4 * PI, true);
+    // and also varies based on the particle's location
+    let angle = xAngle * (x / width) + yAngle * (y / height);
 
-  ellipse( 50,  50, radi, radi);
-  ellipse(150,  50, radi, radi);
-  ellipse( 50, 150, radi, radi);
-  ellipse(150, 150, radi, radi);
+    // each particle moves in a circle
+    let myX = x + 20 * cos(2 * PI * t + angle);
+    let myY = y + 20 * sin(2 * PI * t + angle);
+
+    ellipse(myX, myY, 10); // draw particle
+  })
+
+  t = t + 0.01; // update time
 }
