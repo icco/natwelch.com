5d8d2d23b9e93692e455433628b490e0932da4ad
Nat Welch <nat@natwelch.com>
2018-06-27 12:46:31 +0000

    update ganal

--- js/nat.js
+++ js/nat.js
@@ -16,15 +16,6 @@ var Nat = {
   },
 };
 
-// Google Anal
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'UA-333449-2', 'auto');
-ga('send', 'pageview');
-
 // Add spinning logo
 let t = 0
 let rand = []
@@ -74,3 +65,10 @@ function draw() {
 function round(x) {
   return Number.parseFloat(x).toFixed(4);
 }
+
+// Google
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'UA-333449-2');
