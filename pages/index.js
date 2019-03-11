9b8cb6c642e35c238609988119b0a663c0f871d7
Nat Welch <nat@natwelch.com>
2019-03-11 02:28:00 +0000

    whee

--- pages/index.js
+++ pages/index.js
@@ -2,45 +2,49 @@ import Link from "next/link";
 
 import Age from "../components/Age.js";
 
+const h2_class = "lh-title f2-l f3 fw1 tracked pa3 ma0 near-black bg-moon-gray";
+const p_class = "f4 f3-l measure w8-l mt3 mr0 ph3 near-black";
+const link_class = "black link";
+
 export default () => (
   <section className="cf lh-copy">
     <div className="fl-l wi-50-l ph3 pv4 pa3-m pa5-l">
-      <h2>
+      <h2 className={h2_class}>
         <Link href="/about">
-          <a>About</a>
+          <a className={link_class}>About »</a>
         </Link>
       </h2>
-      <p className="f3 f2-l measure w8-l mt3">
-        Nat is <Age /> year old from the San Francisco Bay Area, currently
+      <p className={p_class}>
+        Nat was born in the San Francisco Bay Area <Age /> ago. He currently is
         living in Brooklyn and working as a Site Reliability Engineer for
         Google's Customer Reliability Engineering team.
       </p>
 
-      <h2>
+      <h2 className={h2_class}>
         <Link href="/talks">
-          <a>Talks</a>
+          <a className={link_class}>Talks »</a>
         </Link>
       </h2>
-      <p className="f3 f2-l measure w8-l mt3">
+      <p className={p_class}>
         Promoting communication, creativity and sharing on the Internet.
       </p>
 
-      <h2>
+      <h2 className={h2_class}>
         <Link href="/projects">
-          <a>Building</a>
+          <a className={link_class}>Building »</a>
         </Link>
       </h2>
-      <p className="f3 f2-l measure w8-l mt3">
+      <p className={p_class}>
         Automating things and building software tools to make maintaining the
         internet easier.
       </p>
 
-      <h2>
+      <h2 className={h2_class}>
         <Link href="/writing">
-          <a>Writing</a>
+          <a className={link_class}>Writing »</a>
         </Link>
       </h2>
-      <p className="f3 f2-l measure w8-l mt3">
+      <p className={p_class}>
         Advocating for transparency, remix culture and open source software.
       </p>
     </div>
