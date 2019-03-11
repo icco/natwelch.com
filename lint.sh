880826f61dc2e8254597b2a58b44dcee4d0e9011
Nat Welch <nat@natwelch.com>
2019-03-11 01:22:26 +0000

    remove static frm lint

--- lint.sh
+++ lint.sh
@@ -4,7 +4,7 @@
 
 yarn
 
-jsfiles=$(git ls-tree --name-only -r HEAD | grep -e '.js$' -e '.json$' -e '.css$')
+jsfiles=$(git ls-tree --name-only -r HEAD | grep -v "^static/" | grep -e '.js$' -e '.json$' -e '.css$')
 [ -z "$jsfiles" ] && exit 0
 
 echo "$jsfiles" | xargs $(yarn bin)/prettier --write
