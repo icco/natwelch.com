e0b720b6075e3eaded59a7255d5cc88a70bd6bc2
Nat Welch <nat@natwelch.com>
2019-03-18 02:11:34 +0000

    about

--- components/TextHeader.js
+++ components/TextHeader.js
@@ -1,8 +1,8 @@
 export default params => {
   const headers = {
-    "1": <h1 className="f3 f2-m f1-l">{params.childre}</h1>,
-    "2": <h2 className="f4 f3-m f2-l">{params.childre}</h2>,
-    "3": <h3 className="f5 f4-m f3-l">{params.childre}</h3>,
+    "1": <h1 className="f3 f2-m f1-l">{params.children}</h1>,
+    "2": <h2 className="f4 f3-m f2-l">{params.children}</h2>,
+    "3": <h3 className="f5 f4-m f3-l">{params.children}</h3>,
   };
 
   let level = "1";
--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 
+import Age from "../components/Age.js";
 import Header from "../components/Header.js";
 import TextHeader from "../components/TextHeader.js";
 
@@ -9,22 +10,44 @@ export default () => (
       <title>Nat Welch | About</title>
     </Head>
     <Header noLogo />
-    <section className="cf lh-copy">
+    <section className="cf lh-copy pa3 pa5-ns">
       <TextHeader level="1">About</TextHeader>
-      <article className="pa3 pa5-ns">
-        <TextHeader level="2">Speaker Bio</TextHeader>
+      <article className="">
+        <TextHeader level="2">Nat Welch</TextHeader>
         <p className="measure lh-copy">
-          Nat Welch is an Site Reliability Engineer based in Brooklyn, NY, and
-          the author of "Real World SRE" from Packt Publishing. He has been
-          professionally building and running software on the internet since
-          2005, and loves making tools and promoting conversation online. He
-          currently works for Google on the Customer Reliability Engineering
-          team. In the past, he has worked for First Look Media, Hillary for
-          America, iFixit, and others.
+          Hi there! I'm Nat. I was born in the San Francisco Bay Area, <Age />{" "}
+          ago. I currently live in Brooklyn with my wife Melissa and our cat
+          Beasley. During the day I work as a Site Reliability Engineer for
+          Google's Customer Reliability Engineering team.
         </p>
+
+        <p className="measure lh-copy">
+          I have some core beliefs that are the basis for most things that I do.
+        </p>
+
+        <ul className="list pa0">
+          <li className="mv3 pl3 bl bw2 b--blue">
+            Sharing information is the most important action in human society.
+          </li>
+
+          <li className="mv3 pl3 bl bw2 b--blue">
+            I love automating things and building software tools to make
+            maintaining the internet easier.
+          </li>
+
+          <li className="mv3 pl3 bl bw2 b--blue">
+            Writing software is like writing English (or whatever language you
+            prefer).
+          </li>
+
+          <li className="mv3 pl3 bl bw2 b--blue">
+            Humans are inherently good and hilarious, and communicating with
+            them well is important.
+          </li>
+        </ul>
       </article>
 
-      <article className="ph3 ph5-ns">
+      <article className="">
         <TextHeader level="2">Nat around the world</TextHeader>
         <p className="measure lh-copy">
           I have lived in San Francisco, London, New York and a few other
@@ -68,6 +91,19 @@ export default () => (
             </a>
           </li>
         </ul>
+      </article>
+
+      <article className="">
+        <TextHeader level="2">Speaker Bio</TextHeader>
+        <p className="measure lh-copy">
+          Nat Welch is an Site Reliability Engineer based in Brooklyn, NY, and
+          the author of "Real World SRE" from Packt Publishing. He has been
+          professionally building and running software on the internet since
+          2005, and loves making tools and promoting conversation online. He
+          currently works for Google on the Customer Reliability Engineering
+          team. In the past, he has worked for First Look Media, Hillary for
+          America, iFixit, and others.
+        </p>
       </article>
     </section>
   </>
