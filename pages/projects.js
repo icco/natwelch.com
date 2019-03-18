f8358f503bdb4da9d639bf14ff7d7d632e04327f
Nat Welch <nat@natwelch.com>
2019-03-18 01:19:37 +0000

    Progress

--- pages/projects.js
+++ pages/projects.js
@@ -1,5 +1,7 @@
-import Header from "../components/Header.js";
 import Head from "next/head";
+
+import Header from "../components/Header.js";
+import TextHeader from "../components/TextHeader.js";
 
 export default () => (
   <>
@@ -7,6 +9,13 @@ export default () => (
       <title>Nat Welch | Projects</title>
     </Head>
     <Header noLogo />
-    <section className="cf lh-copy" />
+    <section className="cf lh-copy pa3 pa5-ns">
+      <TextHeader level="1">Projects</h1>
+
+      <article className="">
+        
+      <TextHeader level="2">GraphQL</h1>
+  </article>
+    </section>
   </>
 );
