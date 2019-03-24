d30704ed2dc66ea2a311941dc72187cd7268d094
Nat Welch <nat@natwelch.com>
2019-03-24 17:42:23 +0000

    project process

--- components/ProjectData.js
+++ components/ProjectData.js
@@ -1,4 +1,4 @@
-import Datetime from "Datetime"
+import Datetime from "./Datetime";
 
 const ProjectQuery = `
 query {
@@ -17,29 +17,35 @@ https://api.github.com/graphql
 {
   "authorization": "bearer 28b1423ce7d34b3b660ae38029f9b6910fd9268d"
 }
-`
+`;
 
-export default (params) => {
+export default params => {
+  if (params.owner == "" || params.repo == "") {
+    return <div className={params.className}>&nbsp;</div>;
+  }
+
   return (
     <div className={params.className}>
-    <div class="cf">
-    <dl class="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
-      <dd class="f6 fw4 ml0">Last Commit</dd>
-      <dd class="f3 fw6 ml0"><Datetime>2019-03-17T03:58:10Z</Datetime></dd>
-    </dl>
-    <dl class="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
-      <dd class="f6 fw4 ml0">Watchers</dd>
-      <dd class="f3 fw6 ml0">3</dd>
-    </dl>
-    <dl class="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
-      <dd class="f6 fw4 ml0">Stars</dd>
-      <dd class="f3 fw6 ml0">22</dd>
-    </dl>
-    <dl class="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
-      <dd class="f6 fw4 ml0">Forks</dd>
-      <dd class="f3 fw6 ml0">4</dd>
-    </dl>
-    </div>
+      <div className="cf">
+        <dl className="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
+          <dd className="f6 fw4 ml0">Last Commit</dd>
+          <dd className="f3 fw6 ml0">
+            <Datetime>2019-03-17T03:58:10Z</Datetime>
+          </dd>
+        </dl>
+        <dl className="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
+          <dd className="f6 fw4 ml0">Watchers</dd>
+          <dd className="f3 fw6 ml0">3</dd>
+        </dl>
+        <dl className="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
+          <dd className="f6 fw4 ml0">Stars</dd>
+          <dd className="f3 fw6 ml0">22</dd>
+        </dl>
+        <dl className="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
+          <dd className="f6 fw4 ml0">Forks</dd>
+          <dd className="f3 fw6 ml0">4</dd>
+        </dl>
+      </div>
     </div>
   );
 };
--- pages/projects.js
+++ pages/projects.js
@@ -5,6 +5,8 @@ import ProjectData from "../components/ProjectData.js";
 import ProjectLogo from "../components/ProjectLogo.js";
 import TextHeader from "../components/TextHeader.js";
 
+let article_class = "cf mw7 bg-white br3 pv1 ph3 pv2-ns mv3 ba b--black-10";
+
 export default () => (
   <>
     <Head>
@@ -24,66 +26,150 @@ export default () => (
         favorite projects that I have or am working on.
       </p>
 
-      <article className="">
-        <TextHeader level="2">GraphQL</TextHeader>
-        <ProjectLogo name="graphql" />
+      <article className={article_class}>
+        <TextHeader level="2" className="dib mt1 mb2">
+          GraphQL
+        </TextHeader>
+        <ProjectLogo name="graphql" className="db fr-ns" />
+        <p className="lh-copy measure f6 black-70">
+          GraphQL is my my server for storing data for use in my projects. It's
+          hosted at{" "}
+          <a href="https://graphql.natwelch.com">graphql.natwelch.com</a> and
+          the code is at{" "}
+          <a href="https://github.com/icco/graphql">github.com/icco/graphql</a>.
+        </p>
 
-        <p className="measure">
-  GraphQL is my my server for storing data for use in my projects. It's hosted at <a href="https://graphql.natwelch.com">graphql.natwelch.com</a> and the code is at <a href="https://github.com/icco/graphql">.</p>
-
-      <ProjectData owner="icco" repo="graphql" />
+        <ProjectData owner="icco" repo="graphql" />
       </article>
 
-      <article className="">
-        <TextHeader level="2">Google Cloud Status</TextHeader>
+      <article className={article_class}>
+        <TextHeader level="2" className="dib mt1 mb2">
+          Google Cloud Status
+        </TextHeader>
+        <ProjectLogo name="" className="db fr-ns" />
+        <p className="lh-copy measure f6 black-70" />
+
+        <ProjectData owner="icco" repo="" />
       </article>
 
-      <article className="">
-        <TextHeader level="2">sadnat.com</TextHeader>
+      <article className={article_class}>
+        <TextHeader level="2" className="dib mt1 mb2">
+          sadnat.com
+        </TextHeader>
+        <ProjectLogo name="sadnat" className="db fr-ns" />
+        <p className="lh-copy measure f6 black-70" />
+
+        <ProjectData owner="icco" repo="sadnat.com" />
       </article>
 
-      <article className="">
-        <TextHeader level="2">Giftionary</TextHeader>
+      <article className={article_class}>
+        <TextHeader level="2" className="dib mt1 mb2">
+          Giftionary
+        </TextHeader>
+        <ProjectLogo name="giftionary" className="db fr-ns" />
+        <p className="lh-copy measure f6 black-70" />
+
+        <ProjectData owner="icco" repo="giftionary" />
       </article>
 
-      <article className="">
-        <TextHeader level="2">Inspiration</TextHeader>
+      <article className={article_class}>
+        <TextHeader level="2" className="dib mt1 mb2">
+          Inspiration
+        </TextHeader>
+        <ProjectLogo name="inspiration" className="db fr-ns" />
+        <p className="lh-copy measure f6 black-70" />
+
+        <ProjectData owner="icco" repo="inspiration" />
       </article>
 
-      <article className="">
-        <TextHeader level="2">Fog</TextHeader>
+      <article className={article_class}>
+        <TextHeader level="2" className="dib mt1 mb2">
+          Fog
+        </TextHeader>
+        <ProjectLogo name="fog" className="db fr-ns" />
+        <p className="lh-copy measure f6 black-70" />
+
+        <ProjectData owner="fog" repo="fog" />
       </article>
 
-      <article className="">
-        <TextHeader level="2">GoTak</TextHeader>
+      <article className={article_class}>
+        <TextHeader level="2" className="dib mt1 mb2">
+          GoTak
+        </TextHeader>
+        <ProjectLogo name="gotak" className="db fr-ns" />
+        <p className="lh-copy measure f6 black-70" />
+
+        <ProjectData owner="icco" repo="gotak" />
       </article>
 
-      <article className="">
-        <TextHeader level="2">Time</TextHeader>
+      <article className={article_class}>
+        <TextHeader level="2" className="dib mt1 mb2">
+          Time
+        </TextHeader>
+        <ProjectLogo name="time" className="db fr-ns" />
+        <p className="lh-copy measure f6 black-70" />
+
+        <ProjectData owner="icco" repo="time" />
       </article>
 
-      <article className="">
-        <TextHeader level="2">Onesie</TextHeader>
+      <article className={article_class}>
+        <TextHeader level="2" className="dib mt1 mb2">
+          Onesie
+        </TextHeader>
+        <ProjectLogo name="onesie" className="db fr-ns" />
+        <p className="lh-copy measure f6 black-70" />
+
+        <ProjectData owner="icco" repo="onesie-be" />
       </article>
 
-      <article className="">
-        <TextHeader level="2">new_job</TextHeader>
+      <article className={article_class}>
+        <TextHeader level="2" className="dib mt1 mb2">
+          new_job
+        </TextHeader>
+        <ProjectLogo name="newjob" className="db fr-ns" />
+        <p className="lh-copy measure f6 black-70" />
+
+        <ProjectData owner="icco" repo="new_job" />
       </article>
 
-      <article className="">
-        <TextHeader level="2">bloomFilter</TextHeader>
+      <article className={article_class}>
+        <TextHeader level="2" className="dib mt1 mb2">
+          bloomFilter
+        </TextHeader>
+        <ProjectLogo name="bloomfilter" className="db fr-ns" />
+        <p className="lh-copy measure f6 black-70" />
+
+        <ProjectData owner="icco" repo="bloomfilter" />
       </article>
 
-      <article className="">
-        <TextHeader level="2">Coffee Shop</TextHeader>
+      <article className={article_class}>
+        <TextHeader level="2" className="dib mt1 mb2">
+          Coffee Shop
+        </TextHeader>
+        <ProjectLogo name="" className="db fr-ns" />
+        <p className="lh-copy measure f6 black-70" />
+
+        <ProjectData owner="icco" repo="coffee_shop" />
       </article>
 
-      <article className="">
-        <TextHeader level="2">three-things</TextHeader>
+      <article className={article_class}>
+        <TextHeader level="2" className="dib mt1 mb2">
+          three-things
+        </TextHeader>
+        <ProjectLogo name="" className="db fr-ns" />
+        <p className="lh-copy measure f6 black-70" />
+
+        <ProjectData owner="icco" repo="three-things" />
       </article>
 
-      <article className="">
-        <TextHeader level="2">instagram-continued</TextHeader>
+      <article className={article_class}>
+        <TextHeader level="2" className="dib mt1 mb2">
+          instagram-continued
+        </TextHeader>
+        <ProjectLogo name="" className="db fr-ns" />
+        <p className="lh-copy measure f6 black-70" />
+
+        <ProjectData owner="icco" repo="instagram-continued" />
       </article>
     </section>
   </>
