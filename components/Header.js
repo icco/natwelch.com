5af6215dfe7a74f34265cc933a4574aa5fbb0eba
Nat Welch <nat@natwelch.com>
2019-03-28 00:04:27 +0000

    better css

--- components/Header.js
+++ components/Header.js
@@ -31,25 +31,25 @@ class Header extends React.Component {
       nav = (
         <>
           <Link href="/about">
-            <a className="link f6 dib mr3 mr4-ns" style={{ color: "#000000" }}>
+            <a className="f6 dib mr3 mr4-ns">
               About
             </a>
           </Link>
 
           <Link href="/projects">
-            <a className="link f6 dib mr3 mr4-ns" style={{ color: "#000000" }}>
+            <a className="f6 dib mr3 mr4-ns">
               Projects
             </a>
           </Link>
 
           <Link href="/writing">
-            <a className="link f6 dib mr3 mr4-ns" style={{ color: "#000000" }}>
+            <a className="f6 dib mr3 mr4-ns">
               Writing
             </a>
           </Link>
 
           <Link href="/talks">
-            <a className="link f6 dib mr3 mr4-ns" style={{ color: "#000000" }}>
+            <a className="f6 dib mr3 mr4-ns">
               Talks
             </a>
           </Link>
