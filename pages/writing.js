5eaae60071270c5418dd5d7e1b7e1785a318c57e
Nat Welch <nat@natwelch.com>
2019-04-08 23:48:49 +0000

    Build writing page

--- pages/writing.js
+++ pages/writing.js
@@ -1,5 +1,7 @@
-import Header from "../components/Header.js";
 import Head from "next/head";
+
+import Header from "../components/Header.js";
+import TextHeader from "../components/TextHeader.js";
 
 export default () => (
   <>
@@ -7,6 +9,43 @@ export default () => (
       <title>Nat Welch | Writing</title>
     </Head>
     <Header noLogo />
-    <section className="cf lh-copy" />
+    <section className="cf lh-copy pa3 pa5-ns">
+      <TextHeader level="1">Writing</TextHeader>
+
+      <p className="f5 f4-ns measure w8-ns mt3">
+        I write once or twice a week at writing.natwelch.com.
+      </p>
+
+      <p className="f5 f4-ns measure w8-ns mt3">
+        I have also written for other places:
+      </p>
+
+      <ul className="list f5 f4-ns mw8 mt3 pl0">
+        <li className="ml2 mv4">
+          <a href="https://www.packtpub.com/web-development/real-world-sre">
+            Real World SRE
+          </a>{" "}
+          from Packt Publishing.
+        </li>
+        <li className="ml2 mv4">
+          Git Out The Vote:{" "}
+          <a href="https://medium.com/git-out-the-vote/chatops-198f0b62c601">
+            #chatops
+          </a>
+        </li>
+
+        <li className="ml2 mv4">
+          Code Words:{" "}
+          <a href="https://codewords.recurse.com/issues/six/promoting-reliability-in-web-software-companies">
+            Promoting reliability in web software companies
+          </a>{" "}
+          &amp;{" "}
+          <a href="https://codewords.recurse.com/issues/three/ddos-and-you">
+            DDoS and You
+          </a>
+          .
+        </li>
+      </ul>
+    </section>
   </>
 );
