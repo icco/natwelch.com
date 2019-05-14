d31c745ba0d93f124ab7314fe3b6ab76af62b6a3
Nat Welch <nat@natwelch.com>
2019-05-14 10:57:16 +0000

    get rid of gtag

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Head, Main, NextScript } from "next/document";
-import { GA_TRACKING_ID } from "../lib/gtag";
 import { TRACKING_ID } from "../lib/fathom";
 
 export default class WritingDocument extends Document {
@@ -14,21 +13,6 @@ export default class WritingDocument extends Document {
           />
           <meta charSet="utf-8" />
 
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-          />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}');
-          `,
-            }}
-          />
           <script
             dangerouslySetInnerHTML={{
               __html: `
