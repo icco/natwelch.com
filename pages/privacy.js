99e2764a313493d79293ac5443758b8a91594dc9
Nat Welch <nat@natwelch.com>
2019-06-22 20:24:13 +0000

    privacy policy update

--- pages/privacy.js
+++ pages/privacy.js
@@ -21,6 +21,12 @@ export default () => (
           address. We reserve the right to keep this data indefinitely. Please
           email nat@natwelch.com if you would like your information removed.
         </p>
+
+        <p>
+          Data is stored on a database hosted in the US on Google Cloud
+          Platform. We also utilize Google Analytics on some sites and Auth0 on
+          others.
+        </p>
       </article>
     </section>
   </App>
