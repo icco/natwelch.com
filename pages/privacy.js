86c61c59b614890e6c459131528bd90b71dcf139
Nat Welch <nat@natwelch.com>
2019-06-22 20:21:51 +0000

    privacy policy

---
+++ pages/privacy.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+
+import Age from "../components/Age.js";
+import App from "../components/App";
+import Header from "../components/Header.js";
+import TextHeader from "../components/TextHeader.js";
+
+export default () => (
+  <App>
+    <Head>
+      <title>Nat Welch | Privacy Policy</title>
+    </Head>
+    <Header noLogo />
+    <section className="cf lh-copy pa3 pa5-ns">
+      <TextHeader level="1">Privacy Policy</TextHeader>
+      <article className="lh-copy f4-ns f5 measure-wide">
+  <p>
+        If you visit any subdomain of natwelch.com, I will store your IP address, visit time, and pages visited. If you login or create an account with natwelch.com or its subdomains, we will store your email address. We reserve the right to keep this data indefinitely. Please email nat@natwelch.com if you would like your information removed.
+  </p>
+      </article>
+    </section>
+  </App>
+);
