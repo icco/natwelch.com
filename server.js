e9dff0fa75f35e8c6a3c428832d7a96cd24d1cf2
Nat Welch <nat@natwelch.com>
2019-07-05 16:53:46 +0000

    simplify

--- server.js
+++ server.js
@@ -1,36 +1,22 @@
+const express = require("express");
 const fs = require("fs");
-const { createServer } = require("http");
-const { parse } = require("url");
+const helmet = require("helmet");
 const next = require("next");
-const { join } = require("path");
 
-const port = parseInt(process.env.PORT, 10) || 8080;
 const dev = process.env.NODE_ENV !== "production";
+const port = parseInt(process.env.PORT, 10) || 3000;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url, true);
-    let path = join(__dirname, "static", parsedUrl.pathname);
-    if (fs.existsSync(path)) {
-      let stat = fs.lstatSync(path);
-      if (stat.isFile()) {
-        app.serveStatic(req, res, path);
-        return;
-      }
+  const server = express();
+  server.use(helmet());
 
-      if (stat.isDirectory()) {
-        let index = join(path, "index.html");
-        if (fs.existsSync(index)) {
-          app.serveStatic(req, res, path);
-          return;
-        }
-      }
-    }
+  server.get("*", (req, res) => {
+    return handle(req, res);
+  });
 
-    handle(req, res, parsedUrl);
-  }).listen(port, err => {
+  server.listen(port, err => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${port}`);
   });
