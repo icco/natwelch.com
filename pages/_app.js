1f29ee3f4e5ac3328f69a529857e855f7fa15301
Nat Welch <nat@natwelch.com>
2020-11-18 02:00:51 +0000

    cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,28 @@
-import App from "next/app";
+import Router from "next/router";
+import Head from "next/head";
 
 import "../style.css";
 
-export default class Home extends App {
-  render() {
-    const { Component, pageProps, apolloClient } = this.props;
-    return <Component {...pageProps} />;
-  }
+function WWW({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="viewport-fit=cover" />
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+          key="viewport"
+        />
+      </Head>
+              <Component {...pageProps} />
+</>
+  );
 }
+
+// Will be called once for every metric that has to be reported.
+export function reportWebVitals(metric) {
+  // These metrics can be sent to any analytics service
+  console.log(metric);
+}
+
+export default WWW;
--- pages/_document.js
+++ pages/_document.js
@@ -1,41 +1,14 @@
-import Document, { Head, Main, NextScript } from "next/document";
-import { TRACKING_ID } from "../lib/fathom";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class WritingDocument extends Document {
   render() {
     return (
-      <html lang="en">
-        <Head>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-            key="viewport"
-          />
-          <meta charSet="utf-8" />
-
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-            (function(f, a, t, h, o, m){
-              a[h]=a[h]||function(){
-                (a[h].q=a[h].q||[]).push(arguments)
-              };
-              o=f.createElement('script'),
-              m=f.getElementsByTagName('script')[0];
-              o.async=1; o.src=t; o.id='fathom-script';
-              m.parentNode.insertBefore(o,m)
-            })(document, window, '//a.natwelch.com/tracker.js', 'fathom');
-            fathom('set', 'siteId', '${TRACKING_ID}');
-            fathom('trackPageview');
-          `,
-            }}
-          />
-        </Head>
+      <Html lang="en">
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
