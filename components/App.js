aa2e95a90fe3a52d917256b161f61e39310c28a9
Nat Welch <nat@natwelch.com>
2020-11-18 02:15:32 +0000

    more stripping

--- components/App.js
+++ components/App.js
@@ -1,10 +1,5 @@
 import React from "react";
 import Router from "next/router";
-import * as fathom from "../lib/fathom";
-
-Router.onRouteChangeComplete = (url) => {
-  fathom.pageview(url);
-};
 
 const App = ({ children }) => <main>{children}</main>;
 
--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   serverRuntimeConfig: {
     // Will only be available on the server side
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN,
   },
   publicRuntimeConfig: {
     // Will be available on both server and client
-    GITHUB_TOKEN: process.env.GITHUB_TOKEN,
   },
 };
