fd59bd267650207f9f1deb0f9ca2a6ea8037710c
Nat Welch <nat@natwelch.com>
2020-12-26 05:42:36 +0000

    nope

--- pages/2021/dashboard.js
+++ pages/2021/dashboard.js
@@ -52,10 +52,4 @@ const Stat = ({keyString, value}) => (
   </dl>
 )
 
-export async function getServerSideProps(context) {
-  return {
-    props: {}, // will be passed to the page component as props
-  }
-}
-
 export default Dashboard;
