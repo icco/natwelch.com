ff71d765278db7c7616b87c7885b8bd85b71b35b
Nat Welch <nat@natwelch.com>
2021-01-11 02:02:43 +0000

    bandwidth calc

---
+++ pages/2021/bandwidth.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Head from "next/head";
+
+import App from "components/App";
+import Header from "components/Header.js";
+
+const Bandwidth = () => {
+
+  return (
+    <App>
+      <Head>
+        <title>Nat Welch | Bandwidth Calculator</title>
+      </Head>
+      <Header noLogo />
+
+    </App>
+  );
+};
+
+export default Bandwidth;
