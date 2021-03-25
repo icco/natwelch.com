02b074b552898c878a14bf3af4dfda11c6f507d5
Nat Welch <nat@natwelch.com>
2021-03-25 18:31:45 +0000

    lint

--- pages/support.js
+++ pages/support.js
@@ -14,16 +14,22 @@ const Support = () => (
       <TextHeader level="1">Support</TextHeader>
 
       <p className="f5 f4-ns measure-wise mt3">
-  If you would like to support my open source work, there are a few ways you can help me out.
+        If you would like to support my open source work, there are a few ways
+        you can help me out.
       </p>
 
       <ul className="list f5 f4-ns wide mt3 pl0">
         <li className="mv3 pl3 bl bw2 b--beige">
           Buy my book <a href="https://realworldsre.com/">Real-World SRE</a>.
         </li>
-          Buy my book <a href="https://www.newline.co/courses/reliable-webservers-with-go/">Reliable Webservers with Go</a>.
+        Buy my book{" "}
+        <a href="https://www.newline.co/courses/reliable-webservers-with-go/">
+          Reliable Webservers with Go
+        </a>
+        .
         <li className="mv3 pl3 bl bw2 b--beige">
-  Support me montly on <a href="https://github.com/sponsors/icco">Github</a>.
+          Support me montly on{" "}
+          <a href="https://github.com/sponsors/icco">Github</a>.
         </li>
       </ul>
     </section>
