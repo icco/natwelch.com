4eb2a9abba6830230a49a2c0fbe72846e3d37055
Nat Welch <nat@natwelch.com>
2022-01-28 14:16:39 +0000

    rewrite header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,133 +2,143 @@ import { Logo } from "@icco/react-common";
 import { TextHeaderOne, TextHeaderTwo } from "components/TextHeader";
 import Link from "next/link";
 import React from "react";
-import { Flex } from "theme-ui";
 
-class Header extends React.Component<{ noLogo?: boolean; navtext?: string }> {
-  render() {
-    let prefix = <></>;
-    let nav = <></>;
-    let head = (
-      <>
-        <header sx={{ py: 0, px: [3, 4, 5], mt: 5 }}>
-          <div sx={{ display: "table", w: "100%" }}>
-            <div
+const links = [
+  ["About", "/wiki/about"],
+  ["Wiki", "/wiki"],
+  ["Resume", "https://resume.natwelch.com"],
+];
+
+export function SmallHeader() {
+  return (
+    <header
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        variant: "styles.header",
+      }}
+    >
+      <Link href="/">
+        <a>
+          <Logo
+            size={50}
+            sx={{
+              verticalAlign: "middle",
+              py: 0,
+              my: [null, 1],
+              display: "inline-block",
+              textAlign: "center",
+              stroke: "text",
+            }}
+            className="logo"
+          />
+        </a>
+      </Link>
+      <div sx={{ mx: "auto" }} />
+
+      {links.map(([name, link]) => {
+        return (
+          <Link key={name} href={link}>
+            <a
               sx={{
-                display: "table-cell",
-                width: "50%",
-                verticalAlign: "middle",
+                variant: "styles.navlink",
+                p: 2,
               }}
             >
-              <Logo
-                size={200}
-                sx={{
-                  verticalAlign: "middle",
-                  py: 0,
-                  my: [null, 1],
-                  display: "inline-block",
-                  textAlign: "center",
-                  stroke: "primary",
-                }}
-              />
-              <div
-                sx={{
-                  display: "inline-block",
-                  verticalAlign: "middle",
-                  p: [0, 3],
-                }}
-              >
-                <TextHeaderOne>Nat Welch</TextHeaderOne>
-                <TextHeaderTwo>Software Reconnaissance Engineer</TextHeaderTwo>
-              </div>
-            </div>
-          </div>
-        </header>
-      </>
-    );
+              {name}
+            </a>
+          </Link>
+        );
+      })}
+    </header>
+  );
+}
 
-    if (this.props.noLogo) {
-      head = <></>;
-      nav = (
+export function SmallHeaderWithText({ navtext }) {
+  return (
+    <header
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        variant: "styles.header",
+      }}
+    >
+      <Link href="/">
+        <a>
+          <Logo
+            size={50}
+            sx={{
+              verticalAlign: "middle",
+              py: 0,
+              my: [null, 1],
+              display: "inline-block",
+              textAlign: "center",
+              stroke: "text",
+            }}
+            className="logo"
+          />
+        </a>
+      </Link>
+      <div sx={{ mx: "auto" }} />
+      <TextHeaderOne sx={{ mr: 4 }}>{navtext}</TextHeaderOne>
+    </header>
+  );
+}
+
+export function LargeHeader() {
+  return (
+    <header sx={{ py: 0, px: [3, 4, 5], mt: 5 }}>
+      <div sx={{ display: "table", w: "100%" }}>
         <div
           sx={{
-            display: "flex",
-            alignItems: "center",
-            variant: "styles.header",
+            display: "table-cell",
+            width: "50%",
+            verticalAlign: "middle",
           }}
         >
-          <div sx={{ mx: "auto" }} />
-
-          <Link href="/wiki/about">
-            <a
-              sx={{
-                variant: "styles.navlink",
-                p: 2,
-              }}
-            >
-              About
-            </a>
-          </Link>
-
-          <Link href="/wiki">
-            <a
-              sx={{
-                variant: "styles.navlink",
-                p: 2,
-              }}
-            >
-              Wiki
-            </a>
-          </Link>
-
-          <Link href="https://resume.natwelch.com">
-            <a
-              sx={{
-                variant: "styles.navlink",
-                p: 2,
-              }}
-            >
-              Resume
-            </a>
-          </Link>
+          <Logo
+            size={200}
+            sx={{
+              verticalAlign: "middle",
+              py: 0,
+              my: [null, 1],
+              display: "inline-block",
+              textAlign: "center",
+              stroke: "primary",
+            }}
+          />
+          <div
+            sx={{
+              display: "inline-block",
+              verticalAlign: "middle",
+              p: [0, 3],
+            }}
+          >
+            <TextHeaderOne>Nat Welch</TextHeaderOne>
+            <TextHeaderTwo>Software Reconnaissance Engineer</TextHeaderTwo>
+          </div>
         </div>
-      );
-      prefix = (
-        <Link href="/">
-          <a>
-            <Logo
-              size={50}
-              sx={{
-                verticalAlign: "middle",
-                py: 0,
-                my: [null, 1],
-                display: "inline-block",
-                textAlign: "center",
-                stroke: "text",
-              }}
-              className="logo"
-            />
-          </a>
-        </Link>
-      );
-    }
+      </div>
+    </header>
+  );
+}
 
-    if (this.props.navtext) {
-      nav = (
-        <>
-          <TextHeaderOne sx={{ mr: 4 }}>{this.props.navtext}</TextHeaderOne>
-        </>
-      );
-    }
+export enum Size {
+  Small = "small",
+  Medium = "medium",
+  Large = "large",
+}
 
-    return (
-      <header>
-        <Flex as="nav">
-          <Flex>{prefix}</Flex>
-          {nav}
-        </Flex>
-        {head}
-      </header>
-    );
+class Header extends React.Component<{ size: Size; navtext?: string }> {
+  render() {
+    switch (this.props.size) {
+      case Size.Large:
+        return <LargeHeader />;
+      case Size.Medium:
+        return <SmallHeaderWithText navtext={this.props.navtext} />;
+      case Size.Small:
+        return <SmallHeader />;
+    }
   }
 }
 
