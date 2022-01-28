993ddc58bc6c3087e7e207e5e6254df3eff257bc
Nat Welch <nat@natwelch.com>
2022-01-28 13:31:21 +0000

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,18 @@
 import { DateTime } from "luxon";
 
 export default function Footer() {
-  return <footer>&copy; Nat Welch {DateTime.now().year}</footer>;
+  return (
+    <footer
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        alignItems: "center",
+        p: 2,
+        variant: "styles.footer",
+      }}
+    >
+      <div sx={{ mx: "auto" }} />
+      <div sx={{ p: 2 }}>&copy; Nat Welch {DateTime.now().year}</div>
+    </footer>
+  );
 }
