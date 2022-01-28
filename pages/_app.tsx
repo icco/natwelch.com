b35ab5042b5f53876c870035efdaa0502277f26a
Nat Welch <nat@natwelch.com>
2022-01-28 02:40:48 +0000

    style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,25 @@
-import "style.css";
-
+import Theme from "components/Theme";
 import Head from "next/head";
+import { ThemeProvider } from "theme-ui";
 
-function WWW({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <meta name="viewport" content="viewport-fit=cover" />
-        <meta
-          name="viewport"
-          content="initial-scale=1.0, width=device-width"
-          key="viewport"
-        />
-        <link
-          rel="webmention"
-          href="https://webmention.io/natwelch.com/webmention"
-        />
-        <link rel="pingback" href="https://webmention.io/natwelch.com/xmlrpc" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
-}
+export const App = ({ Component, pageProps }) => (
+  <ThemeProvider theme={Theme}>
+    <Head>
+      <meta name="viewport" content="viewport-fit=cover" />
+      <meta
+        name="viewport"
+        content="initial-scale=1.0, width=device-width"
+        key="viewport"
+      />
+      <link
+        rel="webmention"
+        href="https://webmention.io/natwelch.com/webmention"
+      />
+      <link rel="pingback" href="https://webmention.io/natwelch.com/xmlrpc" />
+    </Head>
+    <Component {...pageProps} />
+  </ThemeProvider>
+);
 
 // Will be called once for every metric that has to be reported.
 export function reportWebVitals(metric) {
@@ -35,5 +33,3 @@ export function reportWebVitals(metric) {
     fetch(url, { body, method: "POST", keepalive: true });
   }
 }
-
-export default WWW;
