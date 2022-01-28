87e3b0104c7c4e3a05a65b49abad682712afa370
Nat Welch <nat@natwelch.com>
2022-01-28 02:45:07 +0000

    renders

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,24 +2,26 @@ import Theme from "components/Theme";
 import Head from "next/head";
 import { ThemeProvider } from "theme-ui";
 
-export const App = ({ Component, pageProps }) => (
-  <ThemeProvider theme={Theme}>
-    <Head>
-      <meta name="viewport" content="viewport-fit=cover" />
-      <meta
-        name="viewport"
-        content="initial-scale=1.0, width=device-width"
-        key="viewport"
-      />
-      <link
-        rel="webmention"
-        href="https://webmention.io/natwelch.com/webmention"
-      />
-      <link rel="pingback" href="https://webmention.io/natwelch.com/xmlrpc" />
-    </Head>
-    <Component {...pageProps} />
-  </ThemeProvider>
-);
+export default function App({ Component, pageProps }) {
+  return (
+    <ThemeProvider theme={Theme}>
+      <Head>
+        <meta name="viewport" content="viewport-fit=cover" />
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width"
+          key="viewport"
+        />
+        <link
+          rel="webmention"
+          href="https://webmention.io/natwelch.com/webmention"
+        />
+        <link rel="pingback" href="https://webmention.io/natwelch.com/xmlrpc" />
+      </Head>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
 
 // Will be called once for every metric that has to be reported.
 export function reportWebVitals(metric) {
