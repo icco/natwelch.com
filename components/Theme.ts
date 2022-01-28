03738245b283769f0924674a6baa502f34dacd06
Nat Welch <nat@natwelch.com>
2022-01-28 02:28:33 +0000

    theme

--- components/Theme.ts
+++ components/Theme.ts
@@ -1,24 +1,64 @@
-import { ThemeUIContextValue } from 'theme-ui'
-import { Theme } from 'theme-ui'
+import { Nord } from "@icco/react-common"
+import { toTheme } from "@theme-ui/typography"
+import merge from "lodash.merge"
+import fairyGates from "typography-theme-fairy-gates"
 
+const typography = toTheme(fairyGates)
+const root = {
+  config: {
+    initialColorModeName: "light",
+    useColorSchemeMediaQuery: true,
+    printColorModeName: "print",
+  },
 
-interface ExactContextValue extends Omit<ThemeUIContextValue, 'theme'> {
-  theme: ExactTheme
-}
-
-export const useTheme = (useThemeUI as unknown) as () => ExactContextValue
-
-const makeTheme = <T extends Theme>(t: T) => t
-
-const theme = makeTheme({
   colors: {
-    background: 'white',
-    text: 'black',
-    blue: {
-      light: '#187abf',
-      dark: '#235a97',
+    text: Nord.nord3,
+    background: "#fff",
+    primary: Nord.nord4,
+    secondary: Nord.nord1,
+    link: Nord.nord10,
+    modes: {
+      dark: {
+        text: Nord.nord6,
+        background: Nord.nord0,
+        primary: Nord.nord1,
+        secondary: Nord.nord4,
+        link: Nord.nord9,
+      },
+      print: {
+        background: "#fff",
+        text: "#000",
+        primary: Nord.nord1,
+        secondary: Nord.nord4,
+        link: Nord.nord10,
+      },
     },
   },
-})
 
-export type ExactTheme = typeof theme
+  styles: {
+    root: {
+      color: "text",
+      bg: "background",
+      p: [3, 4],
+    },
+    footer: {
+      fontSize: 1,
+    },
+  },
+
+  buttons: {
+    primary: {
+      cursor: "pointer",
+    },
+  },
+
+  "@media print": {
+    fontSize: 1,
+    p: 0,
+    width: "100%",
+  },
+}
+
+const theme = merge(typography, root)
+
+export default theme
