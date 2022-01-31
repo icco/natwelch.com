b771639f189ab0b3398894a5ea15ea54adc20d60
Nat Welch <nat@natwelch.com>
2022-01-31 12:33:52 +0000

    progress

--- components/Lists.tsx
+++ components/Lists.tsx
@@ -76,7 +76,7 @@ const Tree = ({ items }) => {
   );
 };
 
-// Inspo: https://codepen.io/alphardex/pen/WNvLvgX
+// Inspo: https://github.com/alphardex/aqua.css/blob/master/src/breadcrumb.scss
 const Breadcrumbs = ({ path }) => {
   const pieces = path.split("/");
   return (
@@ -87,6 +87,49 @@ const Breadcrumbs = ({ path }) => {
           margin: 0,
           padding: 0,
           listStyleType: "none",
+
+          a: {
+            position: "relative",
+            textDecoration: "none",
+
+            "&::before": {
+              position: "absolute",
+              content: "",
+              bottom: 0,
+              left: 0,
+              width: "100%",
+              height: "unit(0.25)",
+              background: "currentColor",
+              transform: "scaleX(0)",
+              transition: "0.3s",
+            },
+
+            "&:hover::before": {
+              transform: "scaleX(1)",
+            },
+          },
+
+          "&:not(:first-child)": {
+            paddingLeft: "unit(2.25)",
+
+            "&::before": {
+              content: "/",
+              paddingRight: "unit(2.25)",
+              color: "text",
+            },
+          },
+
+          "&:not(:last-child)": {
+            a: {
+              color: "secondary",
+            },
+          },
+
+          "&:last-child": {
+            a: {
+              color: "primary",
+            },
+          },
         }}
       >
         {pieces.map((piece: string, index: number) => {
