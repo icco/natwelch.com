8c2b3ef7ccb864af9deec19626eef65f891d520b
Nat Welch <nat@natwelch.com>
2022-02-05 02:32:49 +0000

    add query

---
+++ lib/graphql.ts
@@ -0,0 +1,21 @@
+import { createClient } from 'urql'
+
+export const client = createClient({
+  url: 'https://graphql.natwelch.com/graphql',
+})
+
+const latestPostQuery = `
+  query latestPostQuery {
+    posts(input: { limit: 1}) {
+      id
+      uri
+      title
+    }
+  }
+`
+
+export const getLatestBlogPost = async () => {
+  const { data: { posts } } = await client.query(latestPostQuery).toPromise()
+
+  return posts[0]
+}
