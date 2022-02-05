d3f1e1b84b98324605265697cdb2b371a5ec982b
Nat Welch <nat@natwelch.com>
2022-02-05 02:50:54 +0000

    style

--- components/BlogPost.tsx
+++ components/BlogPost.tsx
@@ -3,7 +3,13 @@ import Link from "next/link";
 const BlogPost = (params) => {
   const { post } = params;
   return (
-    <div sx={{}}>
+    <div
+      sx={{
+        m: "2em",
+        display: "flex",
+        alignContent: "center",
+      }}
+    >
       <Link passHref href={post.uri}>
         <a>{post.title}</a>
       </Link>
