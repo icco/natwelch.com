2fbcc8a9c0dd6049004c69b05c11b59bc2a02496
Nat Welch <nat@natwelch.com>
2023-10-29 15:57:36 +0000

    fhcnage

--- components/Breadcrumbs.tsx
+++ components/Breadcrumbs.tsx
@@ -3,8 +3,6 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-import "./breadcrumbs.module.css"
-
 // Inspo: https://github.com/alphardex/aqua.css/blob/master/src/breadcrumb.scss
 export const Breadcrumbs = () => {
   const path = usePathname();
@@ -14,12 +12,12 @@ export const Breadcrumbs = () => {
   });
   return (
     <nav className="flex mr-8">
-      <ol className="inline-flex items-center space-x-1 md:space-x-3">
+      <ol className="flex items-center space-x-1 md:space-x-3">
         {pieces.map((piece: string, index: number) => {
           return (
             <li
               key={piece}
-              className="inline-flex items-center text-sm font-medium breadcrumbs"
+              className="breadcrumbs"
             >
               <Link href={`/${pieces.slice(0, index + 1).join("/")}`}>
                 {piece}
