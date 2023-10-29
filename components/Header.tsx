207350594a268dc50c6352b93659f165b3fd3b68
Nat Welch <nat@natwelch.com>
2023-10-29 14:32:55 +0000

    sure

--- components/Header.tsx
+++ components/Header.tsx
@@ -40,7 +40,7 @@ export function LargeHeader() {
 
   return (
     <header>
-      <div>TBD</div>
+      <div><Logo size={200} /></div>
       <div>
         <h1>Nat Welch</h1>
         <h2>Software Reconnaissance Engineer</h2>
