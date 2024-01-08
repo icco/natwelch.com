c997cd40d180285b0ab46a62fdca7d87debb1e15
Nat Welch <nat@natwelch.com>
2024-01-08 02:46:53 +0000

    lint

--- components/Age.tsx
+++ components/Age.tsx
@@ -2,12 +2,15 @@ import { formatDistance, formatISO9075 } from "date-fns";
 import { zonedTimeToUtc } from "date-fns-tz";
 
 export default function Age() {
-  const birth = zonedTimeToUtc("1988-02-22 17:30:00.000", "America/Los_Angeles");
+  const birth = zonedTimeToUtc(
+    "1988-02-22 17:30:00.000",
+    "America/Los_Angeles"
+  );
   const now = new Date();
 
   const diff = formatDistance(birth, now, { addSuffix: true });
   if (birth && now && diff) {
-    const birthString = formatISO9075(birth)
+    const birthString = formatISO9075(birth);
     return <time dateTime={birthString}>{diff}</time>;
   }
 
