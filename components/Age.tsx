7bff788a4d146d6d8466f95fed61ec28f317c8b3
Nat Welch <nat@natwelch.com>
2024-01-08 02:54:13 +0000

    Revert "one date fun"
    This reverts commit a3aa233233d102e7ec99ee25fc5d00d07a4cc130.

--- components/Age.tsx
+++ components/Age.tsx
@@ -1,17 +1,15 @@
-import { formatDistance, formatISO9075 } from "date-fns";
-import { zonedTimeToUtc } from "date-fns-tz";
+import { DateTime } from "luxon";
 
 export default function Age() {
-  const birth = zonedTimeToUtc(
-    "1988-02-22 17:30:00.000",
-    "America/Los_Angeles"
+  const birth = DateTime.fromObject(
+    { year: 1988, month: 2, day: 22, hour: 17 },
+    { zone: "America/Los_Angeles" }
   );
-  const now = new Date();
-
-  const diff = formatDistance(birth, now, { addSuffix: true });
+  const now = DateTime.now();
+  const diff = now.diff(birth, "years", { conversionAccuracy: "longterm" });
   if (birth && now && diff) {
-    const birthString = formatISO9075(birth);
-    return <time dateTime={birthString}>{diff}</time>;
+    const birthString = birth.toISO() ?? "";
+    return <time dateTime={birthString}>{diff.toHuman()} ago</time>;
   }
 
   return <></>;
