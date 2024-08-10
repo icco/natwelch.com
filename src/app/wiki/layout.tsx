e16f04418a6b6b349dc2cf28f2a763d68e4b33b4
Nat Welch <nat@natwelch.com>
2024-08-10 04:43:36 +0000

    types

--- src/app/wiki/layout.tsx
+++ src/app/wiki/layout.tsx
@@ -1,5 +1,5 @@
-import Footer from "components/Footer";
-import { Header, Size } from "components/Header";
+import Footer from "@/components/Footer";
+import { Header, Size } from "@/components/Header";
 
 export default function DashboardLayout({
   children,
--- src/components/Lists.tsx
+++ src/components/Lists.tsx
@@ -3,7 +3,15 @@ import Link from "next/link";
 
 import { Page } from "contentlayer/generated";
 
-function Tree({ items }: { items: string | Record<string, Page> }) {
+function Tree({ items }: { items: string | Record<string, Page> | Page }) {
+  if (isString(items)) {
+    return <>{items}</>;
+  }
+
+  if (items instanceof Page) {
+    return Page;
+  }
+
   return (
     <ul key={`ul-${uniqueId()}`} className="ms-4 list-none">
       {Object.keys(items).map((k) => {
