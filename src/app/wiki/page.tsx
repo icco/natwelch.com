b50efec43220737019286e1db525e1bbe6a0acac
Nat Welch <nat@natwelch.com>
2024-09-03 15:35:19 -0400

    Add Header Images (#124)
    Some cleanup and a new image type

--- src/app/wiki/page.tsx
+++ src/app/wiki/page.tsx
@@ -1,56 +1,15 @@
-import { merge } from "lodash";
 import { Metadata } from "next";
 
-import { Tree } from "@/components/Lists";
+import { buildTree, getPaths, Tree } from "@/components/Lists";
 
-import { allPages, Page } from "contentlayer/generated";
-
-function getPaths(): string[] {
-  const paths: string[] = allPages.map((page) => page.path);
-  return paths.sort();
-}
-
-function buildTree(
-  filter?: (value: string) => boolean
-): Record<string, Page> | string {
-  let tree = {};
-  let paths = getPaths();
-  if (filter) {
-    paths = paths.filter(filter);
-  }
-
-  paths.forEach((path) => {
-    tree = merge(tree, buildTreeInt(path, path));
-  });
-
-  return tree;
-}
-
-function buildTreeInt(
-  path: string,
-  fullPath: string
-): Record<string, Page> | string {
-  if (!path.includes("/")) {
-    const data = allPages.find((page) => page.path === fullPath);
-    if (!data) {
-      throw new Error(`Could not find page for ${path}`);
-    }
-
-    return { [path]: data };
-  }
-
-  const pieces = path.split("/");
-  return {
-    [pieces[0]]: buildTreeInt(pieces.slice(1).join("/"), fullPath),
-  } as unknown as Record<string, Page> | string;
-}
+import { allPages } from "contentlayer/generated";
 
 export const metadata: Metadata = {
   title: "Nat Welch | Wiki",
 };
 
 export default function Home() {
-  const tree = buildTree();
+  const tree = buildTree(getPaths(allPages), allPages);
 
   return (
     <section className="prose lg:prose-xl">
--- wiki/music.md
+++ wiki/music.md
@@ -2,6 +2,8 @@
 title: Music
 ---
 
+<HeaderImage src="https://icco.imgix.net/photos/2024/0H5NJ8THPTA49.jpg" alt="A eurorack modular synthesizer" />
+
 I make electronic music in my free time as a hobby. I've tried lots of different systems for making music (Ableton, Livecoding, Op-1, Volcas, etc) in the past, but currently my system of choice which is really resonating with me is a combination of [Monome Norns Shield](https://monome.org/docs/norns/shield/) and [Eurorack](https://en.wikipedia.org/wiki/Eurorack).
 
 I put practice videos occasionally on [YouTube/@icco](https://www.youtube.com/@icco).
