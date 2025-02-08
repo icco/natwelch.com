e919951953644efc6a7e7e1f377f435ee939a9df
Nat Welch <nat@natwelch.com>
2025-02-08 22:17:09 +0000

    pin

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,35 +9,6 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   // eslint-disable-next-line @typescript-eslint/no-require-imports
-  plugins: [require("@tailwindcss/typography"), require("daisyui")],
-  daisyui: {
-    themes: [
-      {
-        light: {
-          // eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports
-          ...require("daisyui/src/theming/themes")["nord"],
-        },
-        dark: {
-          // eslint-disable-next-line @typescript-eslint/no-var-requires
-          "color-scheme": "dark",
-          primary: "#3B4252",
-          secondary: "#434C5E",
-          accent: "#81A1C1",
-          neutral: "#4C566A",
-          "base-content": "#eceff4",
-          "base-100": "#2E3440",
-          info: "#5E81AC",
-          success: "#A3BE8C",
-          warning: "#D08770",
-          error: "#BF616A",
-          "--rounded-box": "0.4rem",
-          "--rounded-btn": "0.2rem",
-          "--rounded-badge": "0.4rem",
-          "--tab-radius": "0.2rem",
-        },
-      },
-    ],
-    logs: false,
-  },
+  plugins: [require("@tailwindcss/typography")],
 };
 export default config;
