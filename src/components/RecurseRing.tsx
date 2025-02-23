9c528a5ebcebea401edad0cdf2db3a4d902cb2cd
Nat Welch <nat@natwelch.com>
2025-02-23 21:16:42 +0000

    whee

---
+++ src/components/RecurseRing.tsx
@@ -0,0 +1,5 @@
+         // <script src="https://raw.githack.com/Qwuke/recurse-ring/main/static/ring.js" type="text/javascript"></script>
+// <div><a id="rc-ring-prev" href="https://ring.recurse.com/prev?id=45">Prev</a></div>
+// <div><a id="rc-ring-home" data-rc-uuid="b3e98b33-7464-4211-ba0b-5cc38ebb03e9" href="https://ring.recurse.com/">The Recurse Webring</a></div>
+// <div><a id="rc-ring-rand" href="https://ring.recurse.com/rand">Random</a></div>
+// <div><a id="rc-ring-next" href="https://ring.recurse.com/next?id=45">Next</a></div>
