43b1bf1a5d93975942b4bdbed704cb1f63617196
Nat Welch <nat@natwelch.com>
2025-03-19 15:32:54 +0000

    Fix infinite recursion in test setup

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -45,5 +45,24 @@ jest.mock("next/cache", () => ({
 
 // Mock contentlayer/generated
 jest.mock("contentlayer/generated", () => ({
-  allPages: require("./mocks/contentlayer").allPages,
+  allPages: [
+    {
+      _id: "1",
+      _raw: { sourceFilePath: "wiki/test1.md", type: "Page" },
+      type: "Page",
+      title: "Test Page 1",
+      path: "test1",
+      url: "/wiki/test1",
+      body: { raw: "Test content 1" },
+    },
+    {
+      _id: "2",
+      _raw: { sourceFilePath: "wiki/nested/test2.md", type: "Page" },
+      type: "Page",
+      title: "Test Page 2",
+      path: "nested/test2",
+      url: "/wiki/nested/test2",
+      body: { raw: "Test content 2" },
+    },
+  ],
 })) 
