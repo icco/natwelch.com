17add2787ea70519e7c0815bce6441a7c8660905
Nat Welch <nat@natwelch.com>
2025-03-19 15:21:45 +0000

    Add test mocks for contentlayer and globals

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -6,6 +6,19 @@ import React from "react"
 global.TextEncoder = TextEncoder
 global.TextDecoder = TextDecoder as any
 
+// Mock Request and Response globals
+global.Request = class Request {
+  constructor() {
+    return {}
+  }
+} as any
+
+global.Response = class Response {
+  constructor() {
+    return {}
+  }
+} as any
+
 // Mock next/navigation
 jest.mock("next/navigation", () => ({
   useRouter: () => ({
@@ -20,4 +33,9 @@ jest.mock("next/link", () => {
   return function Link({ children, href }: { children: React.ReactNode; href: string }) {
     return React.createElement("a", { href }, children)
   }
-}) 
+})
+
+// Mock contentlayer/generated
+jest.mock("contentlayer/generated", () => ({
+  allPages: require("./mocks/contentlayer").allPages,
+})) 
