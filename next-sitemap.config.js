884faf9fcfdd564713da64668643637880209336
Nat Welch <nat@natwelch.com>
2025-03-19 14:57:19 +0000

    stuff

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  autoLastmod: false,
-  siteUrl: process.env.DOMAIN || "https://natwelch.com",
-  generateRobotsTxt: true,
-  changefreq: "monthly",
-}
