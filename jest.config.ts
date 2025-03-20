add04159b8ed3ea73843135959dcd7584052d12b
Nat Welch <nat@natwelch.com>
2025-03-20 02:07:02 +0000

    lint

--- jest.config.ts
+++ jest.config.ts
@@ -2,8 +2,8 @@
  * For a detailed explanation regarding each configuration property, visit:
  * https://jestjs.io/docs/configuration
  */
-import type {Config} from 'jest';
-import nextJest from 'next/jest.js'
+import type { Config } from "jest"
+import nextJest from "next/jest.js"
 
 const config: Config = {
   clearMocks: true,
@@ -11,14 +11,14 @@ const config: Config = {
   coverageDirectory: "coverage",
   coverageProvider: "v8",
   moduleNameMapper: {
-    '^@/src/(.*)$': '<rootDir>/src/$1',
-    "contentlayer/generated": '<rootDir>/.contentlayer/generated',
+    "^@/src/(.*)$": "<rootDir>/src/$1",
+    "contentlayer/generated": "<rootDir>/.contentlayer/generated",
   },
-};
+}
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-  dir: './',
+  dir: "./",
 })
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
