932c3285e31398b3712667cce7eb4cbb8ddddfc2
Nat Welch <nat@natwelch.com>
2025-03-20 02:21:52 +0000

    fix

--- src/app/page.test.tsx
+++ src/app/page.test.tsx
@@ -1,8 +1,9 @@
-import { expect, test } from 'vitest'
-import { render, screen } from '@testing-library/react'
-import Page from '../app/page'
- 
-test('Page', () => {
+import { render, screen } from "@testing-library/react"
+import { expect, test } from "vitest"
+
+import Page from "../app/page"
+
+test("Page", () => {
   render(<Page />)
-  expect(screen.getByRole('heading', { level: 1, name: 'Home' })).toBeDefined()
+  expect(screen.getByRole("heading", { level: 1, name: "Home" })).toBeDefined()
 })
