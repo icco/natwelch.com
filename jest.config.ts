79721469753f91291b06bc7a40b84e22baf13f69
Nat Welch <nat@natwelch.com>
2025-03-20 02:08:24 +0000

    jest

--- jest.config.ts
+++ jest.config.ts
@@ -14,6 +14,7 @@ const config: Config = {
     "^@/src/(.*)$": "<rootDir>/src/$1",
     "contentlayer/generated": "<rootDir>/.contentlayer/generated",
   },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
 
 const createJestConfig = nextJest({
