9f4abb4eaf6093afb9311f27268f60c41eca3d63
Nat Welch <nat@natwelch.com>
2025-03-20 02:18:49 +0000

    change

--- src/__tests__/page.test.tsx
+++ src/__tests__/page.test.tsx
@@ -1,15 +1,8 @@
-import "@testing-library/jest-dom"
-
-import { render, screen } from "@testing-library/react"
-
-import Page from "../app/page"
-
-describe("Page", () => {
-  it("renders a heading", () => {
-    render(<Page />)
-
-    const heading = screen.getByRole("heading", { level: 1 })
-
-    expect(heading).toBeInTheDocument()
-  })
+import { expect, test } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import Page from '../app/page'
+ 
+test('Page', () => {
+  render(<Page />)
+  expect(screen.getByRole('heading', { level: 1, name: 'Home' })).toBeDefined()
 })
